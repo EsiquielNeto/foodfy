d6c570554d48895f950f42ef08c700345a80aa81
Esiquiel Neto <ziki.neto@gmail.com>
2020-03-24 00:19:31 -0300

    Criado o servidor e aplicado o template com nunjuncks

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
 const server = express();
 
+server.use(express.static('public'));
+server.set('view engine', 'njk');
+
+nunjucks.configure('views', {
+    express: server,
+    autoescape: false,
+    noCache: true,
+});
+
+server.get('/', function(req, res) {
+    res.render('index')
+});
+
+server.get('/about', function(req, res) {
+    res.render('about');
+});
+
+server.get('/recipes', function(req, res) {
+    res.render('recipes')
+});
 
 server.listen(5000, function() {
     console.log('server is running');
